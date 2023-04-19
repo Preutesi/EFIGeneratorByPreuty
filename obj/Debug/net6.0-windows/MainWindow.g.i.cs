e2db09f4cf182e30c5836fd362ea71e9ba4a5151
Preutesi <andrepreutesi@gmail.com>
2023-04-19 17:00:52 +0200

    First commit, nothing new

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace EFIGeneratorByPreuty {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EFIGeneratorByPreuty;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EFIGeneratorByPreuty;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
